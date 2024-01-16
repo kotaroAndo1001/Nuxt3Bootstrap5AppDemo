2d5c747a90aff799df617d09a7811ed5d2fa7d6f
kotaroAndo1001 <146802739+kotaroAndo1001@users.noreply.github.com>
2024-01-16 05:32:35 +0000

    0116

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="title">Ballon D'or 2023年ランキング</h1>
     <h2>〇欧州・海外サッカー</h2>
-    <p>10月30日にフランス・パリで行われた2023年バロンドールセレモニーでバロンドール受賞者が発表された。今回は１位から１５位を紹介します</p>
+    <p>10月30日にフランス・パリで行われた2023年バロンドールセレモニーでバロンドール受賞者が発表された。今回は１位から１５位を紹介します。</p>
 
     <h1>＜1~5位＞</h1>
 
@@ -19,7 +19,7 @@
             <div class="card mb-3">
               <div class="card-body">
                 <h5 class="card-title">リオネル・メッシのプレー動画</h5>
-                <iframe width="100%" height="315" src="https://www.youtube.com/watch?v=fNuL_aSJXyU" frameborder="0" allowfullscreen></iframe>
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/fNuL_aSJXyU" frameborder="0" allowfullscreen></iframe>
               </div>
             </div>
           </div>
@@ -40,7 +40,7 @@
             <div class="card mb-3">
               <div class="card-body">
                 <h5 class="card-title">アーリング・ハーランドのプレー動画</h5>
-                <iframe width="100%" height="315" src="https://www.youtube.com/watch?v=Ib-ZE8_uFVU" frameborder="0" allowfullscreen></iframe>
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/Ib-ZE8_uFVU" frameborder="0" allowfullscreen></iframe>
               </div>
             </div>
           </div>
@@ -49,67 +49,68 @@
     </div>
 
     <!-- 3th Player -->
-    <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-4 text-center">
-          <img src="/images/エンバぺ.jpg" class="img-fluid rounded-start" alt="エンバぺ">
-        </div>
-        <div class="col-md-8">
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4 text-center">
+      <img src="/images/エンバぺ.jpg" class="img-fluid rounded-start" alt="エンバぺ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h2 class="card-title fs-10">3位　キリアン・エンバペ（フランス／パリ・サンジェルマン）</h2>
+        <p class="card-text fs-5">ワールドカップ決勝でのハットトリックはそうそうあるものではない。エンバペはそれをいとも簡単にやってのけた。彼はクラブレベルでも悪くなく、リーグ・アンで29得点を挙げてパリを優勝に導いた。今シーズン、新体制となったPSGで開幕から大活躍しており、来シーズンの受賞も十分に狙えるだろう。</p>
+        <div class="card mb-3">
           <div class="card-body">
-            <h2 class="card-title fs-10">3位　キリアン・エンバペ（フランス／パリ・サンジェルマン）</h2>
-            <p class="card-text fs-5">ワールドカップ決勝でのハットトリックはそうそうあるものではない。エンバペはそれをいとも簡単にやってのけた。彼はクラブレベルでも悪くなく、リーグ・アンで29得点を挙げてパリを優勝に導いた。今シーズン、新体制となったPSGで開幕から大活躍しており、来シーズンの受賞も十分に狙えるだろう。</p>
-            <div class="card mb-3">
-              <div class="card-body">
-                <h5 class="card-title">キリアン・エンバペのプレー動画</h5>
-                <iframe width="100%" height="315" src="https://www.youtube.com/watch?v=AIOiXIxWJvM" frameborder="0" allowfullscreen></iframe>
-              </div>
-            </div>
+            <h5 class="card-title">キリアン・エンバペのプレー動画</h5>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/AIOiXIxWJvM" frameborder="0" allowfullscreen></iframe>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
 
-    <!-- 4th Player -->
-    <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-4 text-center">
-          <img src="images/デブライネ.jpg" class="img-fluid rounded-start" alt="デブライネ">
-        </div>
-        <div class="col-md-8">
+<!-- 4th Player -->
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4 text-center">
+      <img src="/images/デブライネ.jpg" class="img-fluid rounded-start" alt="デブライネ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h2 class="card-title fs-10">4位　ケヴィン・デ・ブライネ（ベルギー／マンチェスター・シティ）</h2>
+        <p class="card-text fs-5">ハーランドはシティの見出しを飾ったが、ペップ・グアルディオラのチームを引っ張ったのは、またしてもデ・ブライネだった。彼らは過去6回のプレミアリーグ優勝のうち5回を制し、2022-23シーズンには3冠を達成している。</p>
+        <div class="card mb-3">
           <div class="card-body">
-            <h2 class="card-title fs-10">4位　ケヴィン・デ・ブライネ（ベルギー／マンチェスター・シティ）</h2>
-            <p class="card-text fs-5">ハーランドはシティの見出しを飾ったが、ペップ・グアルディオラのチームを引っ張ったのは、またしてもデ・ブライネだった。彼らは過去6回のプレミアリーグ優勝のうち5回を制し、2022-23シーズンには3冠を達成している。</p>
-            <div class="card mb-3">
-              <div class="card-body">
-                <h5 class="card-title">ケヴィン・デ・ブライネのプレー動画</h5>
-                <iframe width="100%" height="315" src="https://www.youtube.com/watch?v=Kh6I9wdYpoM" frameborder="0" allowfullscreen></iframe>
-              </div>
-            </div>
+            <h5 class="card-title">ケヴィン・デ・ブライネのプレー動画</h5>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/Kh6I9wdYpoM" frameborder="0" allowfullscreen></iframe>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
 
-    <!-- 5th Player -->
-    <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-4 text-center">
-          <img src="/images/ロドリ.webp" class="img-fluid rounded-start" alt="ロドリ">
-        </div>
-        <div class="col-md-8">
+<!-- 5th Player -->
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4 text-center">
+      <img src="/images/ロドリ.webp" class="img-fluid rounded-start" alt="ロドリ">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h2 class="card-title fs-10">5位 ロドリ（スペイン／マンチェスター・シティ）</h2>
+        <p class="card-text fs-5">10番のように攻撃でき、センターバックのように守れる6番のロドリは、昨年のマン・シティで最も重要な選手だったことは間違いない。チャンピオンズリーグ決勝での決勝ゴールも悪くなかった。</p>
+        <div class="card mb-3">
           <div class="card-body">
-            <h2 class="card-title fs-10">5位 ロドリ（スペイン／マンチェスター・シティ）</h2>
-            <p class="card-text fs-5">10番のように攻撃でき、センターバックのように守れる6番のロドリは、昨年のマン・シティで最も重要な選手だったことは間違いない。チャンピオンズリーグ決勝での決勝ゴールも悪くなかった。</p>
-            <div class="card mb-3">
-              <div class="card-body">
-                <h5 class="card-title">ロドリのプレー動画</h5>
-                <iframe width="100%" height="315" src="https://www.youtube.com/watch?v=w1va55SM-nc" frameborder="0" allowfullscreen></iframe>
-              </div>
-            </div>
+            <h5 class="card-title">ロドリのプレー動画</h5>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/w1va55SM-nc" frameborder="0" allowfullscreen></iframe>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
+
 
   </div>
 </template>
@@ -129,3 +130,6 @@
 }
 
 </style>
+
+<!-- Content Security Policy (CSP) ヘッダー -->
+<meta http-equiv="Content-Security-Policy" content="default-src 'self'; frame-src 'self' https://www.youtube.com; script-src 'self';">
